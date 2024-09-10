cae5322447c0857a0f30e0feebba5681ec5e7261
Avery <anortonsmith@germantownfriends.org>
2024-09-10 08:37:11 -0400

    ddd template

---
+++ Template.java
@@ -0,0 +1,14 @@
+// Import utility classes
+// https://docs.oracle.com/en/java/javase/22/docs/api/java.base/java/util/package-summary.html
+
+import java.util.*;
+
+// Class name should start with an uppercase letter
+// The file name should match the class name. For example Template -> Template.java
+
+class Template {
+  public static void main(String[] args) {
+    // Your code goes here
+    System.out.println("Hello world!");
+  }
+}
