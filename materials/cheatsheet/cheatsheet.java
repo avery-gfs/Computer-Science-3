12b39ddb7944db2227177c37d6ab2459c9892bdd
Avery <anortonsmith@germantownfriends.org>
2024-10-08 13:30:29 -0400

    add cheatsheet

---
+++ materials/cheatsheet/cheatsheet.java
@@ -0,0 +1,438 @@
+// ---- Avery's Java Cheatsheet -----------------------------------
+
+// ---- Template code ---------------------------------------------
+
+import java.util.*; // Import a bunch of useful stuff
+
+class Main { // Should match file name (e.g. Main.java)
+  public static void main(String[] args) {
+    // Your code goes here
+  }
+}
+
+// ---- Variables -------------------------------------------------
+
+int myVar1; // Declaration
+myVar1 = 4; // Assignment
+
+int myVar2 = 5; // Declaration + assignment
+var myVar3 = 6; // Type inferrence
+
+final int x = 1; // Final variables and fields cannot be reassigned
+x = 2; // Error
+
+// ---- Primitives ------------------------------------------------
+
+int i = 10;       // Whole number
+double d = 3.14;  // Decimal number
+boolean b = true; // true or false
+char c = 'x';     // Single character
+
+// Compare primitives with `==` and `!=`
+i == 20 // false
+i !=  0 // true
+
+// Use casts to convert between primitive types
+(double) 2 // 2.0
+(int) 2.8  // 2   (truncates value)
+(int) 'a'  // 97  (unicode value)
+(char) 98  // 'b'
+
+// ---- Math ------------------------------------------------------
+
+int n = 10;
+n + 4          // 14
+n - 4          // 6
+n * 4          // 40
+n / 4          // 2
+n / 4.0        // 2.4
+n % 7          // 3
+Math.pow(n, 4) // 10000.0
+n > 4          // true
+n <= 4         // false
+
+int x = 0;
+x++;    // x = 1
+x += 4; // x = 5
+x--;    // x = 4
+x *= 2; // x = 8
+
+// ---- Strings ---------------------------------------------------
+
+String s = "Hi Java";
+s.equals("Hi Java") // true
+s == "Hi Java"      // Unreliable, use equals()
+s.split(" ")        // String[2] { "Hi", "Java" }
+s.length()          // 7
+s.charAt(5)         // 'v'
+s + " :)"           // "Hi Java :)"
+
+// See also: endsWith, indexOf, repeat, startsWith, strip,
+// toCharArray, toLowerCase, toUpperCase
+
+// ---- Defaults --------------------------------------------------
+
+int defaultInt;        // defaultInt = 0
+defaultInt + 4         // 4
+String defaultString;  // defaultString = null
+defaultString.length() // Error
+
+// ---- Output ----------------------------------------------------
+
+String name = "Avery";
+System.out.println("Hello " + name); // Prints "Hello Avery"
+
+// ---- Input -----------------------------------------------------
+
+Scanner scan = new Scanner(System.in); // New Scanner object
+System.out.print("Enter age: ");       // (optional prompt)
+String name = scan.nextLine();         // Get String input
+System.out.print("Enter age: ");       // (optional prompt)
+int age = scan.nextInt();              // Get int input
+
+// ---- Random ----------------------------------------------------
+
+Random rand = new Random(); // New Random generator object
+rand.nextInt(11)            // Random integer 0 through 10
+rand.nextDouble()           // Random double 0.0 through 1.0
+rand.nextBoolean()          // Random boolean
+
+// ---- File IO ---------------------------------------------------
+
+import java.nio.file.*; // Add this import to the top of your file
+
+// You'll need to add a `throws Exception` annotation to your
+// method or use try/catch to avoid an `unreported exception` error
+
+// Read lines as a List of Strings
+List<String> lines = Files.readAllLines(Path.of("something.txt"));
+
+// Read whole file as a String
+String text = Files.readString(Path.of("something.txt"));
+
+// ---- If --------------------------------------------------------
+
+if (number > 0) {
+  System.out.println("positive");
+} else if (number < 0) {
+  System.out.println("negative");
+} else {
+  System.out.println("zero");
+}
+
+// ---- Ternary ---------------------------------------------------
+
+int number = 7;
+// Choose between two values based on a condition
+String parity = number % 2 == 0 ? "even" : "odd"; // parity = "odd"
+
+// ---- While -----------------------------------------------------
+
+while (n > 10) {
+  // Loop while condition is true
+}
+
+// See also: do-while loops
+
+// ---- For -------------------------------------------------------
+
+for (int i = 1; i <= 10; i++) {
+  // Loop over a range of numbers
+}
+
+for (String name : names) { // `names` can be a List or Array
+  // Loop through a sequence of values
+}
+
+for (String name : names) {
+  if (name.endsWith("y")) {
+    System.out.println(name); // Print the first name ending in 'y'
+    break; // Use `break` to end a `for` or `while` loop early
+  }
+}
+
+// ---- Scope -----------------------------------------------------
+
+int a = 0;
+int b = 0;
+
+if (true) {
+  a = 1;     // Update a in outer scope
+  int b = 1; // New variable b in inner scope
+  int c = 1; // New variable c in inner scope
+}
+
+System.out.println(a); // Prints 1
+System.out.println(b); // Prints 0 (b was only updated inside if)
+System.out.println(c); // Error    (c is only defined inside if)
+
+// ---- Arrays ----------------------------------------------------
+
+String[] dirs = { "north", "south", "east", "west" };
+
+System.out.println(dirs); // Prints nonsense
+
+// Prints { "north", "south", "east", "west" }
+System.out.println(Arrays.toString(dirs));
+
+dirs.length                       // 4
+dirs[0]                           // "north"
+dirs[dirs.length - 1]             // "west"
+dirs[random.nextInt(dirs.length)] // Random direction
+String.join("->", dirs)           // "north->south->east->west"
+
+dirs[0] = "up"; // dirs = { "up", "south", "east", "west" }
+
+// Arrays have a fixed length, no equivalent of Python's `append`
+
+// ---- Lists -----------------------------------------------------
+
+List<String> dirs = List.of("north", "south", "east", "west");
+
+dirs.size()                           // 4
+dirs.get(0)                           // "north" (can't use [0])
+dirs.get(dirs.size() - 1)             // "west"
+dirs.get(random.nextInt(dirs.size())) // Random direction
+String.join("->", dirs)               // "north->south->east->west"
+
+// List.of makes an immutable list that we can't update or add to
+dirs.set(0, "up"); // `UnsupportedOperationException`
+
+// To make a mutable List, use `new ArrayList<>(...)`
+List<String> mutDirs = new ArrayList<>(dirs);
+
+mutDirs.set(0, "up"); // mutDirs = [up, south, east, west]
+mutDirs.add("down");  // mutDirs = [up, south, east, west, down]
+
+// See also: addAll, contains, equals, getLast, indexOf, isEmpty,
+// lastIndexOf, removeLast, reversed, sort, subList
+
+// We can't declare a list of primitives, we have to use wrapper
+// classes like Integer, Double, Boolean, and Character
+
+List<char> letters = List.of('a', 'b', 'c');      // Doesn't work
+List<Character> letters = List.of('a', 'b', 'c'); // Works
+
+// ---- Sets ------------------------------------------------------
+
+Set<Character> letters = new HashSet<>();
+
+letters.add('a');     // letters = [a]
+letters.add('b');     // letters = [a, b]
+letters.size()        // 4
+letters.contains('a') // true
+letters.remove('b');  // letters = [b]
+
+// See also: containsAll, equals, isEmpty, removeAll, retainAll
+
+// ---- Maps ------------------------------------------------------
+
+Map<String, Integer> bank = new HashMap<>();
+
+bank.put("Lamar", 100);     // bank = {Lamar=100}
+bank.put("Zay", 50);        // bank = {Zay=50, Lamar=100}
+bank.put("Lamar", 200);     // bank = {Zay=50, Lamar=200}
+bank.get("Lamar")           // 200
+bank.containsKey("Patrick") // false
+
+// See also: compute, containsValue, entrySet, equals,
+// getOrDefault, isEmpty, keySet, putAll, putIfAbsent, remove,
+// size, values
+
+// ---- Enums -----------------------------------------------------
+
+// Use enums to represent sets of options, like compass directions
+enum Direction {
+  NORTH, SOUTH, EAST, WEST
+}
+
+Direction heading = Direction.NORTH;
+// Compare Enums with `==` and `!=`
+heading != Direction.WEST // true
+
+// ---- Switch ----------------------------------------------------
+
+// Use `switch` to check a value against a set of options
+switch (fileExtension) {
+  case "mp3":
+    System.out.println("It's autio");
+    break; // Need `break`, `return`, or `yield` at end of case
+  case "jpg":
+  case "png":
+  case "webp": // Multiple cases grouped together
+    System.out.println("It's an image");
+    break;
+  default: // Default runs if no other case matches
+    System.out.println("File type unknown");
+}
+
+String arrow = switch (heading) {
+  case NORTH: yield "↑"; // Get a value from `switch` with `yield`
+  case SOUTH: yield "↓";
+  case EAST:  yield "→";
+  case WEST:  yield "←";
+}; // Need semicolon when assigning `switch` result to a variable
+
+// ---- Methods ---------------------------------------------------
+
+static void greet(String name) { // No return value
+  System.out.println("Hello " + name);
+}
+
+static int add(int a, int b) { // Returns an int
+  return a + b;
+}
+
+static double abs(double n) { // Returns a double
+  if (n < 0) {
+    return -n; // Return ends the method immediately
+  }
+
+  return n;
+}
+
+// ---- Overloads -------------------------------------------------
+
+// A method can have multiple variants (overloads) that take
+// different arguments
+
+static int maximum(int a, int b) {
+  // ...
+}
+
+static int maximum(int[] numbers) {
+  // ...
+}
+
+// ---- Varargs ---------------------------------------------------
+
+// Use `...` to allow a flexible number of arguments (varargs)
+static void helloAll(String... names) {
+  // names is a String[]
+  System.out.println("Hello " + String.join(" and ", names));
+}
+
+helloAll("Alice");        // Prints "Hello Alice"
+helloAll("Alice", "Bob"); // Prints "Hello Alice and Bob"
+
+String[] names = { "Alice", "Bob" };
+// You can also pass an Array as varargs
+helloAll(people); // Prints "Hello Alice and Bob"
+
+// ---- Classes ---------------------------------------------------
+
+// Constructor methods make an object from scratch
+// Factory
+// Transformer methods make an object from an old object
+// Mutator methods update and object
+// Observer methods read or calculate data from an object
+
+class Rectangle {
+  double length; // Instance field
+  double width;
+
+  static final int SIDES = 4; // Static constant
+
+  Rectangle(double length, double width) { // Constructor method
+    this.length = length;
+    this.width = width;
+  }
+
+  static Rectangle UnitSquare() { // Factory method
+    return new Rectangle(1, 1);
+  }
+
+  void scale(double factor) { // Mutator method
+    length *= factor;
+    width *= factor;
+  }
+
+  Rectangle rotated() { // Transformer method
+    return new Rectangle(width, length);
+  }
+
+  double area() { // Observer method
+    return length * width;
+  }
+
+  // You can define custom `toString`, `equals`, and `hashCode`
+  // methods to change how a class is displayed, compared, and
+  // hashed, but it's often easier to just use a record class
+  // 
+  // See also: Objects.equals, Objects.hash
+}
+
+Rectangle rect = new Rectangle(4, 5);
+rect.area()     // 20.0
+rect.SIDES      // 4
+Rectangle.SIDES // 4
+
+Rectangle unit = Rectangle.UnitSquare(); // Static method call
+
+// ---- Interfaces ------------------------------------------------
+
+interface Shape {
+  double area();
+}
+
+class Circle implements Shape {
+  double radius;
+
+  Circle(double radius) { this.radius = radius; }
+
+  public double area() {
+    return Math.PI * radius * radius;
+  }
+}
+
+class Rectangle implements Shape {
+  double length;
+  double width;
+
+  Rectangle(double length, double width) {
+    this.length = length;
+    this.width = width;
+  }
+
+  public double area() {
+    return length * width;
+  }
+}
+
+// Different objects can implement same interface (polymorphism)
+List<Shape> shapes = List.of(new Circle(10), new Rectangle(4, 5));
+
+// ---- Records ---------------------------------------------------
+
+// Records are immutable classes with auto-generated constructor,
+// `toString`, `equals`, `hashCode`, and getter methods
+
+record Point(double x, double y) {
+  public double distanceTo(Point other) {
+    double dx = x - other.x;
+    double dy = y - other.y;
+    return Math.sqrt(dx * dx + dy * dy);
+  }
+}
+
+Point p = new Point(0, 3);
+// `x` and `y` are private, use `x()` and `y()` to access fields
+p.x() // 3.0
+p.distanceTo(new Point(4, 0)) // 5.0
+
+// ---- Lambdas ---------------------------------------------------
+
+Map<String, Integer> scores = new HashMap<>();
+scores.put("BAL", 20); // scores = {BAL=20}
+
+// We can use lambdas to update objects
+scores.compute("BAL", (k, v) -> v + 27); // scores = {BAL=27}
+
+// ---- Streams ---------------------------------------------------
+
+List<Integer> numbers = List.of(1, 2, 3, 4, 5);
+
+// We can use Streams to transform sequences
+List<Integer> squares = numbers.stream()
+  .map(n -> n * n)
+  .collect(Collectors.toList()); // [1, 4, 9, 16, 25]
---
+++ syllabus.md
@@ -0,0 +1,73 @@
+# Syllabus: Computer Science 3
+
+## Class Topics, Format, and Philosophy
+
+In previous classes, you've learned the basics of programming and practiced writing software to solve a variety of problems. In this class, we will explore the more theoretical and advanced aspects of programming languages and software engineering. Possible topics include:
+
+- Recursion
+- Static typing and type systems
+- Object oriented, functional, and asynchronous programming
+- Performance analysis and optimization
+- Design principles (modularity, orthogonality, encapsulation, composition, dry, zero-one-infinity, fail fast)
+- Programming language internals (scope, garbage collection, floating point numbers, UTF8, pointers)
+- Programming language development (grammars, parsers, interpreters, compilers, libraries)
+- Algorithms (searching, sorting, greedy algorithms, dynamic programming, string matching)
+- Data structures (graphs, trees, stacks, queues, linked lists, hash tables, persistent data structures)
+
+The class will also prepare you to take the AP Computer Science A exam. As the exam approaches, we'll work on practice problems and review exam-specific information and strategies.
+
+The specific material we cover is somewhat flexible. If there are other topics that you're interested in learning about, let me know and I'll try to incorporate them into the curriculum.
+
+Our work in this class will be centered around a series of projects that each cover a new topic, with a structure that looks something like this:
+
+1. Introduce a new topic
+2. Technical details and demonstration
+3. Project work in class
+4. Out of class work (optional)
+5. Release solutions
+6. Review and discuss solutions
+
+I think that the best way to learn software engineering is by reading and writing software, so we'll be doing a lot of that!
+
+## Grading and Workload
+
+Grading for this class is pass/fail. You don't have to master all the material; as long as you participate in class and do the work, you'll pass. I'll give occasional quizzes as a tool to get feedback on my teaching, but your classwork and participation are what's important.
+
+I know that you all have a lot on your plates, so I don't plan on assigning homework as long as our time in class is focused and productive. However, I will provide lots of optional problems and projects that you can work on outside of class if you want to explore the material on a deeper level.
+
+## AI Tools
+
+Generative AI tools can be a great asset when learning a new language and debugging code, but they're only one of many resources. As a software engineer, it's vital that you learn to utilize a wide set of tools when you encounter challenges, including:
+
+- Official documentation
+- Software Q&A sites
+- Issue trackers and discussions
+- Books and tutorials
+- Reading source code
+- Your teacher and your fellow classmates!
+
+With this in mind, I ask that you not use AI tools for our in-class work. When doing work outside of class-time, you may use AI tools to get feedback on the code you write, but not to write code for you.
+
+Do this:
+
+> I wrote this function (your code here) to check if a number is prime, but it runs forever without returning a result. What am I doing wrong?
+
+Don't do this:
+
+> Write a function that checks if a number is prime.
+
+You may not use Github Copilot, Replit AI, or other "code autocomplete" AI tools when doing work for this class.
+
+## Feedback
+
+I'll try to provide feedback on the code you write for this class. Writing code that works is only the first step of software engineering; the next step is figuring out how to make your code modern, simple, and efficient. My hope is that getting feedback will help you level-up this important aspect of your programming skills.
+
+## Links
+
+[Java language docs](https://docs.oracle.com/en/java/javase/22/docs/api/java.base/java/lang/package-summary.html)
+
+[Java util docs](https://docs.oracle.com/en/java/javase/22/docs/api/java.base/java/util/package-summary.html)
+
+[AP CS textbook](https://runestone.academy/ns/books/published/csawesome/index.html)
+
+[Java for Python programmers](https://runestone.academy/ns/books/published/java4python/index.html?mode=browsing)
