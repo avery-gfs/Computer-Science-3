662dd80c1e5c311a173fd4e95a2e4a885c7a78d4
Avery <anortonsmith@germantownfriends.org>
2024-10-11 12:29:29 -0400

    simplified

--- materials/selection-sort/Main.java
+++ materials/selection-sort/Main.java
@@ -2,44 +2,15 @@ import java.util.*;
 
 class Main {
   public static void main(String[] args) {
-    List<Integer> numbers;
-
-    numbers = new ArrayList<>(List.of(3, 1, 4, 1, 5, 9, 2, 6, 5, 4));
-    System.out.println(isSorted(numbers) == false);
-
-    sort(numbers);
+    List<Integer> numbers = new ArrayList<>(List.of(3, 1, 4, 1, 5, 9, 2, 6, 5, 4));
+    
+    System.out.println(numbers);
     System.out.println(isSorted(numbers));
 
-    numbers = new ArrayList<>();
     sort(numbers);
+
+    System.out.println(numbers);
     System.out.println(isSorted(numbers));
-
-    numbers = new ArrayList<>(List.of(1, 1, 1, 1, 1, 1, 1, 1, 1, 0));
-    sort(numbers);
-    System.out.println(isSorted(numbers));
-
-    numbers = new ArrayList<>(List.of(0, -1, -2));
-    sort(numbers);
-    System.out.println(isSorted(numbers));
-
-    int upper = 10000;
-
-    for (int m = 0; m < 5; m++) {
-      numbers = new ArrayList<>();
-
-      for (int n = 0; n < upper; n++) {
-        numbers.add(n);
-      }
-
-      long start = System.currentTimeMillis();
-      sort(numbers);
-      long end = System.currentTimeMillis();
-
-      System.out.println(isSorted(numbers));
-      System.out.println((end - start) / 1000.0);
-
-      upper *= 2;
-    }
   }
 
   static boolean isSorted(List<Integer> numbers) {
